46ce12ddc3c7c26999b490c3395a3ab5e8ffce78
Andy Grunwald <andygrunwald@gmail.com>
2014-06-22 00:31:04 +0200

    Set version to 0.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andygrunwald@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures tools from *Grimoire (e.g. MetricsGrimoire or VizGrimoire)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0-dev'
+version          '0.0.1'
 
 depends "git"
 depends "python"
